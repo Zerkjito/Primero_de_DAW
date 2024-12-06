9016869edf8da9f373d8be7781e24982e7c4e1f8
Zerkjito <proxy4soon@gmail.com>
2024-12-06 22:36:50 +0100

    Update ej26

--- Asignatura Programación/TEMA 3/Ejercicios figuras/Ejercicio26/src/ejercicio26/Ejercicio26.java
+++ Asignatura Programación/TEMA 3/Ejercicios figuras/Ejercicio26/src/ejercicio26/Ejercicio26.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio26;
+import java.util.Scanner;
 
 /**
  *
@@ -12,9 +13,18 @@ public class Ejercicio26 {
 
     /**
      * @param args the command line arguments
-     */
+     */ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        int filas;
+
+        do {
+            System.out.print("Introduce el numero de filas: ");
+            filas = sc.nextInt();
+        } while (filas < 2 || filas % 2 == 0);
+        
+        for (int i = 1; i <= filas; i++) {
+        }
     }
-    
+
 }
