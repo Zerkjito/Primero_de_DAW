86dfebc8a1de4b321c3798eb9caf991725163037
Zerkjito <proxy4soon@gmail.com>
2025-01-15 11:43:44 +0100

    ejercicios FOL y parking SistemasInf

---
+++ Asignatura Programación/TEMA 4/Ejercicios métodos/JavaApplication53/src/javaapplication53/JavaApplication53.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication53;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Erick
+ */
+public class JavaApplication53 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        System.out.print(n+" ");
+        do {
+            n+=4;
+            System.out.print(n+" ");
+            n-=2;
+            System.out.print(n+" ");
+        } while (n != 23);
+        
+        
+
+    }
+
+}
