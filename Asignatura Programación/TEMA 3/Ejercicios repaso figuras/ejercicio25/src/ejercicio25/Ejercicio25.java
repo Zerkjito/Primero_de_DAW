96b9f1c237b53a1533df1c76bd7e37d8a622ff27
Zerkjito <proxy4soon@gmail.com>
2025-01-28 22:03:45 +0100

    upd ej25

--- Asignatura Programación/TEMA 3/Ejercicios repaso figuras/ejercicio25/src/ejercicio25/Ejercicio25.java
+++ Asignatura Programación/TEMA 3/Ejercicios repaso figuras/ejercicio25/src/ejercicio25/Ejercicio25.java
@@ -17,22 +17,32 @@ public class Ejercicio25 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int filas;
+        int filas, columnas;
         do {
             System.out.print("Introduce filas: ");
             filas = sc.nextInt();
-        } while (filas < 3);
+            System.out.print("Introduce columnas: ");
+            columnas = sc.nextInt();
+        } while (filas % 2 == 0 || filas < 5 || columnas < 5);
         
+        int contador = 1;
+        int centro = filas / 2 + 1;
         for (int i = 1; i <= filas; i++) {
-            for (int j = 1; j <= filas - i; j++) {
-                System.out.print(" ");
+            for (int j =1; j <= columnas; j++) {
+                if (i == 1 && j > 1 && j < columnas || i == filas && j > 1 && j < columnas) {
+                    System.out.print("*");
+                } else if (i == centro) {
+                    System.out.print(contador % 10);
+                } else if (j == 1 || j == columnas) {
+                    System.out.print(contador % 10);
+                } else {
+                    System.out.print(" ");
+                }
             }
-            
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print("*");
-            }
+            contador++;
             System.out.println();
         }
+
     }
     
 }
